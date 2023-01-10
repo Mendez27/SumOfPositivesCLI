e9f8cf24b3558f5e629b3f1c78a0bddc5011d4f2
Mendez27 <edgarmendez2118@gmail.com>
2023-01-10 17:47:40 -0500

    Checkpoint Complete

---
+++ src/main/java/com/galvanize/Main.java
@@ -0,0 +1,21 @@
+package com.galvanize;
+
+public class Main {
+    public static void main(String[] args)
+    {
+       int sum = 0;
+
+        for (int i = 0; i < args.length; i++)
+        {
+        int num = Integer.parseInt(args[i]);
+
+        if (num > 0)
+            {
+                sum += num;
+            }
+        }
+
+        System.out.println(sum);
+
+    }
+}
